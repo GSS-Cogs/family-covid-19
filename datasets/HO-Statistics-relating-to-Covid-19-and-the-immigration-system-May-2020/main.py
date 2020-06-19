e390a1d76d5816cb22f586eb28e9f5e871df9569
Alex Tucker <alex@floop.org.uk>
2020-06-19 14:05:51 +0100

    Try using CSVWMapping and PMD4 metadata export.

--- datasets/HO-Statistics-relating-to-Covid-19-and-the-immigration-system-May-2020/main.py
+++ datasets/HO-Statistics-relating-to-Covid-19-and-the-immigration-system-May-2020/main.py
@@ -32,7 +32,7 @@ tab = next(t for t in tabs.values() if t.name.startswith('Air'))
 print(tab.name)
 
 datacube_name = "HO Statistics relating to Covid-19 and the immigration system"
-columns=['Period', 'Air Arrivals', 'Measure Type', 'Unit']
+columns=['Period', 'Air Arrivals']
 trace.start(datacube_name, tab, columns, scraper.distributions[1].downloadURL)
 
 trace.Period("Selected as non blank values below and including cell A6 and cell E6")
@@ -44,14 +44,9 @@ air_arrivals = tab.excel_ref('B5').expand(RIGHT).is_not_blank() - tab.excel_ref(
 trace.OBS('All non-blank values to the right of Period Values.')
 observations = period.fill(RIGHT).is_not_blank() - period.expand(DOWN)
 
-trace.Measure_Type('Hard Coded to {}', var = 'People')
-trace.Unit('Hard Coded to {}', var = 'Count')
-
 dimensions = [
     HDim(period, 'Period', DIRECTLY, LEFT),
     HDim(air_arrivals, 'Air Arrivals', DIRECTLY, ABOVE),
-    HDimConst('Measure Type', 'People'),
-    HDimConst('Unit', 'Count'),
 ]
 tidy_sheet = ConversionSegment(tab, dimensions, observations)
 trace.with_preview(tidy_sheet)
@@ -75,13 +70,14 @@ df['Air Arrivals'].replace('of which:\nBritish nationals', 'of which British nat
 trace.Period('Removing n/a values')
 df = df[df['Period'] != 'n/a']
             
-tidy = df[['Period', 'Air Arrivals', 'Value', 'Measure Type', 'Unit']]
+tidy = df[['Period', 'Air Arrivals', 'Value']]
 trace.Air_Arrivals("Remove any prefixed whitespace from all values in column and pathify")
 for column in tidy:
     if column in ('Air Arrivals'):
         tidy[column] = tidy[column].str.lstrip()
         tidy[column] = tidy[column].str.rstrip()
         tidy[column] = tidy[column].map(lambda x: pathify(x))
+tidy['Value'] = tidy['Value'].astype(int)
 tidy
 # -
 
@@ -92,21 +88,25 @@ for col in df:
         display(HTML(f"<h2>{col}</h2>"))
         display(df[col].cat.categories) 
 
+# Output Tidy data
+
 out = Path('out')
 out.mkdir(exist_ok=True)
 trace.ALL("Remove all duplicate rows from dataframe.")
 tidy.drop_duplicates().to_csv(out / 'observations.csv', index = False)
 scraper.dataset.family = 'covid-19'
 
-# +
-######### Commented out for now #############
+# Output CSV-W metadata (validation, transform and DSD).
+# Output dataset metadata separately for now.
 
-#from gssutils.metadata import THEME
-
-#with open(out / 'observations.csv-metadata.trig', 'wb') as metadata:
-#    metadata.write(scraper.generate_trig())
-
-#csvw = CSVWMetadata('https://gss-cogs.github.io/family-covid-19/reference/')
-#csvw.create(out / 'observations.csv', out / 'observations.csv-schema.json')
+scraper.set_base_uri('http://gss-data.org.uk')
+scraper.set_dataset_id(f'gss_data/family-covid-19/{pathify(Path(".").absolute().name)}')
+csvw_transform = CSVWMapping()
+csvw_transform.set_csv(out / 'observations.csv')
+csvw_transform.set_mapping(json.load(open('info.json')))
+csvw_transform.set_dataset_uri(scraper.dataset.uri)
+csvw_transform.write(out / 'observations.csv-metadata.json')
+with open(out / 'observations.csv-metadata.trig', 'wb') as metadata:
+    metadata.write(scraper.generate_trig())
 
 trace.output()

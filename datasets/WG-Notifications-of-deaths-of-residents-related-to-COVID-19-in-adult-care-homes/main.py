800e22f28aca56a61f1956d518156cc99e6206c2
Alex Tucker <alex@floop.org.uk>
2020-07-09 18:29:09 +0100

    Start merging tabs.

--- datasets/WG-Notifications-of-deaths-of-residents-related-to-COVID-19-in-adult-care-homes/main.py
+++ datasets/WG-Notifications-of-deaths-of-residents-related-to-COVID-19-in-adult-care-homes/main.py
@@ -18,6 +18,8 @@ list(tabs)
 expected_tabs = ['Contents','Information','Table_1','Table_2','Table_3','Table_4','Table_5','Table_6','Table_7','Table_8', 'Table_9']
 whats_missing = [item for item in tabs if item not in expected_tabs]
 
+tables = {}
+
 # Check firstly there is no tabs missing / removed from latest update
 if  len(whats_missing) != 0:
     print(whats_missing)
@@ -36,10 +38,38 @@ else:
             if file.startswith("'main") == True:
                 continue
             %run $file
+            tables[OBS_ID] = tidy
+# +
+from IPython.core.display import HTML
 
+for key, table in tables.items():
+    display(HTML(f'<h2>{key}</h2>'))
+    display(table)                 
+
+# +
+cubes = Cubes('info.json')
+
+cube1 = tables['number-of-notifications-of-deaths-of-adult-care-home-residents-involving-covid-19-both-confirmed-and-suspected-occurring-in-care-homes-by-local-authority-and-day-of-notification'].copy()
+cube1.drop(columns=['Local Authority', 'Unit'], inplace=True)
+cube1['Notification Date'] = cube1['Notification Date'].map(lambda x: f'day/{x}')
+cube1['Value'] = pd.to_numeric(cube1['Value'], downcast='integer')
+cube1
+
+# +
+cube2 = tables['notifications-of-service-user-deaths-received-from-adult-care-homes'].copy()
+cube2.drop(columns=['Measure Type', 'Unit'], inplace=True)
+cube2.rename(columns={'Notification Date Range': 'Notification Date'}, inplace=True)
+
+from datetime import datetime
+import isodate
+
+def rangeToDuration(r):
+    start, end = [datetime.strptime(d.strip(), '%d/%m/%Y') for d in r.split('-')]
+    return f'gregorian-interval/{start.isoformat()}/{isodate.duration_isoformat(end - start)}'
+    
+cube2['Notification Date'] = cube2['Notification Date'].map(rangeToDuration)
+pd.
+cube2
 # -
-
-
-
 
 

99700421ac82ee93d473d14db60a2854ade55708
Shannon Black <shannon1995black@gmail.com>
2020-07-21 16:44:09 +0100

    Stage 1 Transformation

--- datasets/PHE-COVID-19-number-of-outbreaks-in-care-homes-management-information/main.py
+++ datasets/PHE-COVID-19-number-of-outbreaks-in-care-homes-management-information/main.py
@@ -78,91 +78,83 @@ scrape
 
 tabs = { tab.name: tab for tab in scrape.distributions[0].as_databaker() }
 datasetTitle = 'PHE COVID-19 number of outbreaks in care homes – management information'
-tidied_sheets = []
 trace = TransformTrace()
 df = pd.DataFrame()
 list(tabs)
 
 for name, tab in tabs.items():
+    if 'Metadata' in name:
+        continue
+    elif (name == 'Government_office_regions') or (name == 'PHE_centres'):
+        columns=["Week commencing", "Categorised by", "Region","Area code","All outbreaks", "Number of care homes", "Percentage of care homes that have reported an outbreak", "Measure Type", "Unit"]
+    else:
+        columns=["Week commencing", "Categorised by", "Region","Area code","All outbreaks", "Number of care homes", "Measure Type", "Unit"]
+    trace.start(datasetTitle, tab, columns, scrape.distributions[0].downloadURL)
     
-    datacube_name = "Number of deaths in care homes notified to the Care Quality Commission, England (By Local Authority)"
-    columns=["Week commencing", "Categorised by", "Region","Area code","All outbreaks", "Number of care homes", "Percentage of care homes that have reported an outbreak", "Measure Type", "Unit"]
-    trace.start(datacube_name, tab, columns, scrape.distributions[0].downloadURL)
-
-    if name == 'Government_office_regions':
+    remove_1 = tab.filter(contains_string('All outbreaks')).expand(RIGHT)
+    
+    week_commencing = tab.filter(contains_string('Week commencing')).shift(0,1).expand(RIGHT).is_not_blank() - remove_1
+    trace.Week_commencing('Week commencing given at cell range: {}', var = excelRange(week_commencing))
+    
+    all_outbreaks = tab.filter(contains_string('All outbreaks')).shift(0,1).expand(DOWN).is_not_blank() 
+    trace.All_outbreaks('All outbreaks given at cell range: {}', var = excelRange(all_outbreaks))
+    
+    num_care_homes = tab.filter(contains_string('Number of care homes')).shift(0,1).expand(DOWN).is_not_blank() 
+    trace.Number_of_care_homes('All outbreaks given at cell range: {}', var = excelRange(num_care_homes))
+    
+    if (name == 'Government_office_regions'):
         categorised_by = "Government office region"
         trace.Categorised_by('Hardcoded value as: Government office region')
-        remove_1 = tab.filter(contains_string('All outbreaks')).expand(RIGHT)
         
-        week_commencing = tab.filter(contains_string('Week commencing')).shift(0,1).expand(RIGHT).is_not_blank() - remove_1
-        trace.Week_commencing('Week commencing given at cell range: {}', var = excelRange(week_commencing))
-           
         area_code = tab.filter(contains_string('Government office region')).shift(1,1).expand(DOWN).is_not_blank() 
         trace.Area_code('Area code given at cell range: {}', var = excelRange(area_code))
         
         region = tab.filter(contains_string('Government office region')).shift(0,1).expand(DOWN).is_not_blank() 
         trace.Region('region given at cell range: {}', var = excelRange(region))
-       
-        all_outbreaks = tab.filter(contains_string('All outbreaks')).shift(0,1).expand(DOWN).is_not_blank() 
-        trace.All_outbreaks('All outbreaks given at cell range: {}', var = excelRange(all_outbreaks))
-
-        num_care_homes = tab.filter(contains_string('Number of care homes')).shift(0,1).expand(DOWN).is_not_blank() 
-        trace.Number_of_care_homes('All outbreaks given at cell range: {}', var = excelRange(num_care_homes))
-
+        
         outbreak_percentage = tab.filter(contains_string('Percentage of care homes that have reported an outbreak')).shift(0,1).expand(DOWN).is_not_blank() 
         trace.Percentage_of_care_homes_that_have_reported_an_outbreak('Percentage of care homes that have reported an outbreak given at cell range: {}', var = excelRange(outbreak_percentage))
-
-        measure_type = 'Outbreaks of COVID-19'
-        trace.Measure_Type('Hardcoded value as: Deaths')
-        unit = 'Count'
-        trace.Unit('Hardcoded value as: Count')
-        observations = area_code.waffle(week_commencing)
         
-        dimensions = [
-            HDim(week_commencing, 'Week commencing', DIRECTLY, ABOVE),
-            HDim(area_code, 'Area code', DIRECTLY, LEFT),
-            HDim(region, 'Region', DIRECTLY, LEFT),
-            HDim(all_outbreaks, 'All outbreaks', DIRECTLY, RIGHT),
-            HDim(num_care_homes, 'Number of care homes', DIRECTLY, RIGHT),
-            HDim(outbreak_percentage, 'Percentage of care homes that have reported an outbreak', DIRECTLY, RIGHT),
-            HDimConst('Categorised by', categorised_by),
-            HDimConst('Measure Type', measure_type),
-            HDimConst('Unit', unit),
-        ]
-        tidy_sheet = ConversionSegment(tab, dimensions, observations)
-        trace.with_preview(tidy_sheet)
-        savepreviewhtml(tidy_sheet) 
-        trace.store("combined_dataframe", tidy_sheet.topandas())
-        
-    if name == 'PHE_centres':
+    if (name == 'PHE_centres'):
         categorised_by = "PHE Centres"
         trace.Categorised_by('Hardcoded value as: PHE Centres')
-        remove_1 = tab.filter(contains_string('All outbreaks')).expand(RIGHT)
         
-        week_commencing = tab.filter(contains_string('Week commencing')).shift(0,1).expand(RIGHT).is_not_blank() - remove_1
-        trace.Week_commencing('Week commencing given at cell range: {}', var = excelRange(week_commencing))
-           
         area_code = tab.filter(contains_string('PHE centre')).shift(1,1).expand(DOWN).is_not_blank() 
-        #trace.Area_code('Area code given at cell range: {}', var = excelRange(area_code))
+        trace.Area_code('Area code given at cell range: {}', var = excelRange(area_code))
         
         region = tab.filter(contains_string('PHE centre')).shift(0,1).expand(DOWN).is_not_blank() 
-        #trace.Region('region given at cell range: {}', var = excelRange(region))
-       
-        all_outbreaks = tab.filter(contains_string('All outbreaks')).shift(0,1).expand(DOWN).is_not_blank() 
-        #trace.All_outbreaks('All outbreaks given at cell range: {}', var = excelRange(all_outbreaks))
-
-        num_care_homes = tab.filter(contains_string('Number of care homes')).shift(0,1).expand(DOWN).is_not_blank() 
-        #trace.Number_of_care_homes('All outbreaks given at cell range: {}', var = excelRange(num_care_homes))
-
-        outbreak_percentage = tab.filter(contains_string('Percentage of care homes that have reported an outbreak')).shift(0,1).expand(DOWN).is_not_blank() 
-        #trace.Percentage_of_care_homes_that_have_reported_an_outbreak('Percentage of care homes that have reported an outbreak given at cell range: {}', var = excelRange(outbreak_percentage))
-
-        measure_type = 'Outbreaks of COVID-19'
-        trace.Measure_Type('Hardcoded value as: Deaths')
-        unit = 'Count'
-        trace.Unit('Hardcoded value as: Count')
-        observations = area_code.waffle(week_commencing)
+        trace.Region('region given at cell range: {}', var = excelRange(region))
         
+        outbreak_percentage = tab.filter(contains_string('Percentage of care homes that have reported an outbreak')).shift(0,1).expand(DOWN).is_not_blank() 
+        trace.Percentage_of_care_homes_that_have_reported_an_outbreak('Percentage of care homes that have reported an outbreak given at cell range: {}', var = excelRange(outbreak_percentage))
+    
+    if (name == 'Upper_Tier_Local_authorities'):
+        categorised_by = "Local Authority Upper Tier"
+        trace.Categorised_by('Hardcoded value as: Local Authority Upper Tier')
+        
+        area_code = tab.filter(contains_string('Local Authority')).shift(1,1).expand(DOWN).is_not_blank() 
+        trace.Area_code('Area code given at cell range: {}', var = excelRange(area_code))
+        
+        region = tab.filter(contains_string('Government office region')).shift(0,1).expand(DOWN).is_not_blank() 
+        trace.Region('region given at cell range: {}', var = excelRange(region))
+
+    if (name == 'Lower_Tier_Local_authorities'):
+        categorised_by = "Local Authority Lower Tier"
+        trace.Categorised_by('Hardcoded value as: Local Authority Lower Tier')
+        
+        area_code = tab.filter(contains_string('Local Authority')).shift(1,1).expand(DOWN).is_not_blank() 
+        trace.Area_code('Area code given at cell range: {}', var = excelRange(area_code))
+     
+        region = tab.filter(contains_string('Government office region')).shift(0,1).expand(DOWN).is_not_blank() 
+        trace.Region('region given at cell range: {}', var = excelRange(region))
+    
+    measure_type = 'Outbreaks of COVID-19'
+    trace.Measure_Type('Hardcoded value as: Deaths')
+    unit = 'Count'
+    trace.Unit('Hardcoded value as: Count')
+    observations = area_code.waffle(week_commencing)
+    
+    if ((name == 'Government_office_regions') or (name == 'PHE_centres')):
         dimensions = [
             HDim(week_commencing, 'Week commencing', DIRECTLY, ABOVE),
             HDim(area_code, 'Area code', DIRECTLY, LEFT),
@@ -177,45 +169,46 @@ for name, tab in tabs.items():
         tidy_sheet = ConversionSegment(tab, dimensions, observations)
         trace.with_preview(tidy_sheet)
         savepreviewhtml(tidy_sheet) 
-        trace.store("combined_dataframe", tidy_sheet.topandas())
-
-
+        trace.store("sheets_1_2", tidy_sheet.topandas())
+        
+    elif ((name == 'Upper_Tier_Local_authorities') or (name == 'Lower_Tier_Local_authorities')):
+        dimensions = [
+            HDim(week_commencing, 'Week commencing', DIRECTLY, ABOVE),
+            HDim(area_code, 'Area code', DIRECTLY, LEFT),
+            HDim(region, 'Region', DIRECTLY, LEFT),
+            HDim(all_outbreaks, 'All outbreaks', DIRECTLY, RIGHT),
+            HDim(num_care_homes, 'Number of care homes', DIRECTLY, RIGHT),
+            HDimConst('Categorised by', categorised_by),
+            HDimConst('Measure Type', measure_type),
+            HDimConst('Unit', unit),
+        ]
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        trace.with_preview(tidy_sheet)
+        savepreviewhtml(tidy_sheet) 
+        trace.store("sheets_3_4", tidy_sheet.topandas())
+        
 
 # +
-df = trace.combine_and_trace(datacube_name, "combined_dataframe")
-df.rename(columns={'OBS' : 'Value'}, inplace=True)
-trace.add_column("Value")
-trace.Value("Rename databaker columns OBS to Value")
+df_1_2 = trace.combine_and_trace(datasetTitle, "sheets_1_2")
+df_3_4 = trace.combine_and_trace(datasetTitle, "sheets_3_4")
+tidied_sheets = [df_1_2, df_3_4]
 
-trace.Week_commencing("Formating to gregorian-interval/YYY-MM-DDT00:00:00/P7D")
-df['Week commencing'] =  df["Week commencing"].apply(date_time)
-trace.Week_commencing("Renaming label to Period")
-df.rename(columns={'Week commencing' : 'Period'}, inplace=True)
+for df in tidied_sheets:
+    df.rename(columns={'OBS' : 'Value'}, inplace=True)
+    trace.add_column("Value")
+    trace.Value("Rename databaker columns OBS to Value")
+
+    trace.Week_commencing("Formating to gregorian-interval/YYY-MM-DDT00:00:00/P7D")
+    df['Week commencing'] =  df["Week commencing"].apply(date_time)
+    trace.Week_commencing("Renaming label to Period")
+    df.rename(columns={'Week commencing' : 'Period'}, inplace=True)
 # -
 
-from IPython.core.display import HTML
-for col in df:
-    if col not in ['Value']:
-        df[col] = df[col].astype('category')
-        display(HTML(f"<h2>{col}</h2>"))
-        display(df[col].cat.categories) 
+tidy_1_2 = df_1_2[["Period", 'Value', "Categorised by", "Region","Area code", "Measure Type", "Unit", "All outbreaks", "Number of care homes", "Percentage of care homes that have reported an outbreak"]]
+tidy_3_4 = df_3_4[["Period", 'Value', "Categorised by", "Region","Area code", "Measure Type", "Unit", "All outbreaks", "Number of care homes"]]
 
-tidy = df[["Period", 'Value', "Categorised by", "Region","Area code", "Measure Type", "Unit", "All outbreaks", "Number of care homes", "Percentage of care homes that have reported an outbreak"]]
-tidy
-
-# +
 out = Path('out')
 out.mkdir(exist_ok=True)
 title = pathify(datasetTitle)
-#scraper.dataset.comment = notes
-df.drop_duplicates().to_csv(out / f'{title}.csv', index = False)
-#scraper.dataset.family = 'covid-19'
-
-#df.drop_duplicates().to_csv(out / f'{title}.csv', index = False)
-#with open(out / f'{title}.csv-metadata.trig', 'wb') as metadata:
-#    metadata.write(scraper.generate_trig())
-#trace.output()
-#tidy
-# -
-
-
+tidy_1_2.drop_duplicates().to_csv(out / f'{title}_1.csv', index = False)
+tidy_3_4.drop_duplicates().to_csv(out / f'{title}_2.csv', index = False)

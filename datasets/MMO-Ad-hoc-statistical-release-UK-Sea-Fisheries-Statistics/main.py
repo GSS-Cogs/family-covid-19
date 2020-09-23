4bcc2ac9051009c5e758de447d150aecfeaaf95f
LPerryman <leigh.perryman@gsscogs.uk>
2020-09-23 10:34:04 +0100

    Reformatting date column and changing name to Month from Year

--- datasets/MMO-Ad-hoc-statistical-release-UK-Sea-Fisheries-Statistics/main.py
+++ datasets/MMO-Ad-hoc-statistical-release-UK-Sea-Fisheries-Statistics/main.py
@@ -36,17 +36,38 @@ all_dat.append(tidy)
 # Value and volumn landed by country and vessel length
 # %run "table_4.py"
 all_dat.append(tidy)
-# -
+
+# +
+# Not the best way of getting the dates but hey ho.
+import re
+from time import strptime
+import datetime
+
+mnths = []
+yrs = []
+
+now = datetime.datetime.now()
+yrs.append(now.year - 1)
+yrs.append(now.year)
 
 # Got from Table 1 script
 dateRange = str(dateRange)
-mths = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+mths = ['January','Feburary','March','April','May','June','July','August','September','October','November','December']
+
 for m in mths:
     if m in dateRange:
-        i = dateRange.find(m)
-        mthsStr = dateRange[i:len(dateRange)-3]
-        break
-mthsStr
+        for s in re.finditer(m, dateRange):
+            mnths.append(m)
+
+mnthNo = []
+for m in mnths:
+    s = strptime('Jul','%b').tm_mon
+    if (s < 10):
+        mnthNo.append('0' + str(s))
+    else:
+        mnthNo.append(str(s))
+print(mnthNo)
+print(yrs)
 
 # +
 import numpy as np
@@ -116,7 +137,7 @@ all_dat[3] = all_dat[3][['Period','Country','Vessel Length','Species Group','Adm
 
 joined_dat = pd.concat([all_dat[0],all_dat[1],all_dat[2],all_dat[3]])
 
-joined_dat = joined_dat.rename(columns={'Period':'Year'})
+joined_dat = joined_dat.rename(columns={'Period':'Month'})
 joined_dat['Value'][joined_dat['Marker'] == 'Figure less than 1'] = 0
 #joined_dat["Species Group"] = joined_dat["Species Group"].map(lambda x: fishlookup.get(x, x))
 joined_dat["Species Group"] = joined_dat["Species Group"].str.replace(' total','')
@@ -127,7 +148,7 @@ joined_dat_ton = joined_dat[joined_dat['Unit'] == 'Tonnes']
 print('Ton count 1: ' + str(joined_dat_ton['Country'].count()))
 # Some columns are duplicates but have different rounding on the Value column, get rid based on rest of columns
 print('Getting rid of duplicates')
-joined_dat_ton = joined_dat_ton.drop_duplicates(['Year','Country','Vessel Length','Species Group','Admin Port'], keep='first')
+joined_dat_ton = joined_dat_ton.drop_duplicates(['Month','Country','Vessel Length','Species Group','Admin Port'], keep='first')
 print('Ton count 2: ' + str(joined_dat_ton['Country'].count()))
 joined_dat_ton['Vessel Length'][joined_dat_ton['Vessel Length'] == 'Total'] = 'All'
 joined_dat_ton['Admin Port'][joined_dat_ton['Admin Port'] == 'UK total'] = 'UK'
@@ -139,6 +160,12 @@ joined_dat_ton['Marker'] = joined_dat_ton['Marker'].apply(pathify).str.strip()
 
 del joined_dat_ton['Measure Type']
 del joined_dat_ton['Unit']
+
+joined_dat_ton['Month'][joined_dat_ton['Month'].str.contains(str(yrs[0]))] = 'month/' + str(yrs[0]) + '-' + str(mnthNo[0])
+joined_dat_ton['Month'][joined_dat_ton['Month'].str.contains(str(yrs[1]))] = 'month/' + str(yrs[1]) + '-' + str(mnthNo[1])
+joined_dat_ton['Month'].unique()
+# Realised that this is a monthly figure rather than a yearly one so changing column name here rather than go through everything
+
 # -
 
 for c in joined_dat_ton.columns:

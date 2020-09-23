c9ba24282ed787caf3758d74106158546cd2138a
RedWalters <jonathan.j.walters@Gmail.com>
2020-09-23 13:28:12 +0100

    Fixed assertion error

--- datasets/WG-Attendance-at-local-authority-settings-during-the-coronavirus-COVID-19-pandemic/main.py
+++ datasets/WG-Attendance-at-local-authority-settings-during-the-coronavirus-COVID-19-pandemic/main.py
@@ -1,9 +1,15 @@
-# # WG Attendance at local authority settings during the coronavirus  COVID-19  pandemic 
+#!/usr/bin/env python
+# coding: utf-8
 
-from gssutils import * 
-import json 
+# In[1]:
 
-scrape = Scraper(seed="info.json")   
+
+# # WG Attendance at local authority settings during the coronavirus  COVID-19  pandemic
+
+from gssutils import *
+import json
+
+scrape = Scraper(seed="info.json")
 scrape.distributions[0].title = "Attendance at local authority settings during the coronavirus (COVID-19) pandemic"
 scrape
 
@@ -12,34 +18,35 @@ list(tabs)
 
 next_table = pd.DataFrame()
 
-try : 
+try :
     tab = tabs['Table1_Eng']
     cell = tab.filter('Date')
+    remove = tab.filter(contains_string('Source:'))
     cell.assert_one()
     authority = cell.fill(RIGHT).is_not_blank().is_not_whitespace()
-    date = cell.fill(DOWN).is_not_blank().is_not_whitespace()  
-    observations = authority.fill(DOWN).is_not_blank().is_not_whitespace() 
+    date = cell.fill(DOWN).is_not_blank().is_not_whitespace()
+    observations = authority.fill(DOWN).is_not_blank().is_not_whitespace() - remove
     Dimensions = [
                 HDim(authority,'Local Authority Settings',DIRECTLY,ABOVE),
                 HDim(date, 'Period',DIRECTLY,LEFT),
-                HDimConst('Unit','Count'),  
+                HDimConst('Unit','Count'),
                 HDimConst('Measure Type','Attendence')
 
-    ]  
+    ]
     c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
     new_table = c1.topandas()
     import numpy as np
     new_table.rename(columns={'OBS': 'Value','DATAMARKER': 'Marker'}, inplace=True)
-    
+
     def user_perc1(x,y):
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') : 
+        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
             return 'Percentage'
         else:
             return y
     new_table['Measure Type'] = new_table.apply(lambda row: user_perc1(row['Local Authority Settings'], row['Measure Type']), axis = 1)
-    
-    def user_perc2(x,y):   
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') : 
+
+    def user_perc2(x,y):
+        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
             return 'percent'
         else:
             return y
@@ -48,7 +55,7 @@ try :
     def date_time(time_value):
         time_string = str(time_value).replace(".0", "").strip()
         time_len = len(time_string)
-        if time_len == 10:       
+        if time_len == 10:
             return 'gregorian-day/' + time_string[:10] + 'T00:00:00'
     new_table["Period"] = new_table["Period"].apply(date_time)
     next_table = pd.concat([next_table, new_table])
@@ -58,34 +65,35 @@ except:
 # Period not defined properly due to variation in date format in source data
 # Further updated
 
-try : 
+try :
     tab = tabs['Table2_Eng']
     cell = tab.filter('Week beginning')
+    remove = tab.filter(contains_string('Source:'))
     cell.assert_one()
     authority = cell.shift(1,0).fill(RIGHT).is_not_blank().is_not_whitespace()
-    date = cell.fill(DOWN).is_not_blank().is_not_whitespace()  
-    observations = authority.fill(DOWN).is_not_blank().is_not_whitespace() 
+    date = cell.fill(DOWN).is_not_blank().is_not_whitespace()
+    observations = authority.fill(DOWN).is_not_blank().is_not_whitespace() - remove
     Dimensions = [
                 HDim(authority,'Local Authority Settings',DIRECTLY,ABOVE),
                 HDim(date, 'Period',DIRECTLY,LEFT),
-                HDimConst('Unit','Count'),  
+                HDimConst('Unit','Count'),
                 HDimConst('Measure Type','Attendence')
 
-    ]  
+    ]
     c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
     new_table = c1.topandas()
     import numpy as np
     new_table.rename(columns={'OBS': 'Value','DATAMARKER': 'Marker'}, inplace=True)
-    
+
     def user_perc1(x,y):
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') : 
+        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
             return 'Percentage'
         else:
             return y
     new_table['Measure Type'] = new_table.apply(lambda row: user_perc1(row['Local Authority Settings'], row['Measure Type']), axis = 1)
-    
-    def user_perc2(x,y):   
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') : 
+
+    def user_perc2(x,y):
+        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
             return 'percent'
         else:
             return y
@@ -95,7 +103,7 @@ try :
     def date_time(time_value):
         time_string = str(time_value).replace(".0", "").strip()
         time_len = len(time_string)
-        if time_len == 10:       
+        if time_len == 10:
             return 'gregorian-day/' + time_string[:10] + 'T00:00/P4D'
     new_table["Period"] = new_table["Period"].apply(date_time)
     next_table = pd.concat([next_table, new_table])
@@ -107,10 +115,11 @@ for col in new_table:
     if col not in ['Value']:
         next_table[col] = next_table[col].astype('category')
         display(HTML(f"<h2>{col}</h2>"))
-        display(next_table[col].cat.categories) 
+        display(next_table[col].cat.categories)
 
-tidy = next_table[['Period','Local Authority Settings', 'Measure Type','Unit',  'Value', 'Marker']]
+tidy = next_table[['Period','Local Authority Settings', 'Measure Type','Unit',  'Value']]
 
 out = Path('out')
 out.mkdir(exist_ok=True)
-tidy.drop_duplicates().to_csv(out / 'Attendance at local authority settings during the coronavirus COVID-19 pandemic.csv', index = False)
+tidy.drop_duplicates().to_csv(out / 'Attendance at local authority settings during the coronavirus COVID-19 pandemic_new.csv', index = False)
+

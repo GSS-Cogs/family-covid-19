fd951a860b7190a2e430923d928002b26555ca16
Ajay Joseph <ajay.joseph@ons.gov.uk>
2020-10-06 13:50:17 +0100

    Stage 1 transform after change in dataset

--- datasets/DWP-Universal-Credit-declarations-claims-and-advances-management-information/main.py
+++ datasets/DWP-Universal-Credit-declarations-claims-and-advances-management-information/main.py
@@ -13,8 +13,6 @@ scraper
 tabs = { tab.name: tab for tab in scraper.distribution(latest=True).as_databaker() }
 list(tabs)
 
-df = pd.DataFrame()
-
 
 # +
 def left(s, amount):
@@ -26,7 +24,7 @@ def date_time(time_value):
     time_string = str(time_value).replace(".0", "").strip()
     time_len = len(time_string)
     if time_len == 10:       
-        return 'gregorian-instant/' + time_string[:10] + 'T00:00:00'   
+        return 'gregorian-day/' + time_string[:10]
 
 
 # -
@@ -37,19 +35,30 @@ def date_time(time_value):
 # Table 3	Number of Universal Credit Advances, by type of advance
 # Table 4	Number of Universal Credit Advances Time Periods Totals by type of advance
 #
-# Tables 1 and 3 only transformed as 2 and 4 can be derived. 
+# Tables 1 and 3 only transformed as per spec. Tables 2 and 4 are pulled as it is and all of them is placed in sheets_dictionary.  
 
+# +
+sheets = []
+sheets_dictionary = {}
 for name, tab in tabs.items():
-    if 'Front' in name or 'Contents' in name or 'Notes' in name or 'Definitions' in name or '2' in name or '4' in name:
+    if 'Front' in name or 'Contents' in name or 'Notes' in name or 'Definitions' in name:
+        continue
+    if '2' in name:
+        table2 = tab
+        continue
+    if '4' in name:
+        table4 = tab
         continue
     measure_type = 'Advances' #or Households and Individuals, will be filtered out 
     period = tab.excel_ref('C7').expand(RIGHT).is_not_blank() 
     
     if tab.name == '3':
-        print('now at tab 3')
         period = tab.excel_ref('C8').expand(RIGHT).is_not_blank() 
-
-    univeral_credit_stage = tab.excel_ref('B12').expand(DOWN).is_not_blank() 
+        
+    univeral_credit_stage = tab.excel_ref('B13:B18').is_not_blank() 
+    if tab.name == '1':
+        univeral_credit_stage = tab.excel_ref('B9:B10').is_not_blank() 
+        
     observations = period.fill(DOWN).is_not_blank().is_not_whitespace()
     Dimensions = [
         HDim(period,'Period',DIRECTLY,ABOVE),
@@ -60,32 +69,50 @@ for name, tab in tabs.items():
     c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
     savepreviewhtml(c1, fname=tab.name + "Preview.html")
     new_table = c1.topandas()
-    df = pd.concat([df, new_table], sort=False)
+    sheets.append(new_table)
+
+
+# Process Table 2
+universal_credit_declaration = table2.excel_ref('C7:D7').is_not_blank()
+period = table2.excel_ref('B13:B36').is_not_blank()
+
+obs = table2.excel_ref('C13').expand(DOWN).expand(RIGHT).is_not_blank()
+Dimensions = [
+        HDim(period,'Period', DIRECTLY, LEFT),
+        HDim(universal_credit_declaration, 'Universal Credit Declarations', DIRECTLY, ABOVE)
+    ]
+c2 = ConversionSegment(table2, Dimensions, obs)
+savepreviewhtml(c2)
+sheets_dictionary["2"] = c2.topandas()
+
+# Process table 4
+advance_type = table4.excel_ref('C7:F7').is_not_blank()
+period = table4.excel_ref('B13:B18').is_not_blank()
+
+obs = table4.excel_ref('C13').expand(DOWN).expand(RIGHT).is_not_blank()
+Dimensions = [
+        HDim(period,'Period', DIRECTLY, LEFT),
+        HDim(advance_type, 'Advance Type', DIRECTLY, ABOVE)
+    ]
+c4 = ConversionSegment(table4, Dimensions, obs)
+savepreviewhtml(c4)
+sheets_dictionary["4"] = c4.topandas()
+
+
+dataframe = pd.concat(sheets)
+# Adding advance in front of Universal credit stage, will be removed for rows having measure type Households and Individuals later
+dataframe["Universal Credit Stage"] = dataframe["Universal Credit Stage"].map(lambda x: 'Advance ' + x) 
+# -
 
 
 import numpy as np
-df.rename(columns={'OBS': 'Value'}, inplace=True)
-f1=((df['Universal Credit Stage'] =='Households making a Universal Credit declaration') | (df['Universal Credit Stage'] == 'Individuals making a Universal Credit declaration'))   
-df.loc[f1,'Measure Type'] = 'Households and Individuals'
-
-from IPython.core.display import HTML
-for col in df:
-    if col not in ['Value']:
-        df[col] = df[col].astype('category')
-        display(HTML(f"<h2>{col}</h2>"))
-        display(df[col].cat.categories) 
-
-df["Period"] = df["Period"].apply(date_time)
-for column in df:
-    if column in ('Universal Credit Stage'):
-        df[column] = df[column].str.lstrip()
-        df[column] = df[column].map(lambda x: pathify(x))
-
-tidy = df[['Period', 'Universal Credit Stage', 'Measure Type', 'Unit','Value']]
-
-out = Path('out')
-out.mkdir(exist_ok=True)
-tidy.drop_duplicates().to_csv(out / 'observations.csv', index = False)
+dataframe.rename(columns={'OBS': 'Value'}, inplace=True)
+f1=((dataframe['Universal Credit Stage'] =='Advance Households making a Universal Credit declaration') | (dataframe['Universal Credit Stage'] == 'Advance Individuals making a Universal Credit declaration'))   
+dataframe.loc[f1,'Measure Type'] = 'Households and Individuals'
+dataframe.loc[f1,'Universal Credit Stage'] = dataframe.loc[f1,'Universal Credit Stage'].str.lstrip("Advance ")
+dataframe["Period"] = dataframe["Period"].apply(date_time)
+sheets_dictionary["1 and 3"] = dataframe
+dataframe
 
 # +
 scraper.dataset.family = 'covid-19'
@@ -99,11 +126,3 @@ Figures fewer than 5 are supressed due to rounding to avoid disclosure required 
 
 """
 scraper.dataset.description = scraper.dataset.description + additional_metadata
-
-#from gssutils.metadata import THEME
-#with open(out / 'observations.csv-metadata.trig', 'wb') as metadata:
-    #metadata.write(scraper.generate_trig())
-
-# +
-#csvw = CSVWMetadata('https://gss-cogs.github.io/family-covid-19/reference/')
-#csvw.create(out / 'observations.csv', out / 'observations.csv-schema.json')

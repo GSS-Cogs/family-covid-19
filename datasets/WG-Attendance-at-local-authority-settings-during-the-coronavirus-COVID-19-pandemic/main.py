1d89ebbae42d610c749d4718cb1b8e5069259fb6
RedWalters <jonathan.j.walters@gmail.com>
2020-11-16 13:16:13 +0000

    recoding transform for changed data, spec will need re-doing

--- datasets/WG-Attendance-at-local-authority-settings-during-the-coronavirus-COVID-19-pandemic/main.py
+++ datasets/WG-Attendance-at-local-authority-settings-during-the-coronavirus-COVID-19-pandemic/main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[10]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
 # In[1]:
 
 
@@ -10,21 +16,22 @@ from gssutils import *
 import json
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from lxml import html
 
 info = json.load(open('info.json'))
 
 req = Request(info["landingPage"], headers={'User-Agent': 'Mozilla/5.0'})
-html = urlopen(req).read()
-plaintext = html.decode('utf8')
-soup = BeautifulSoup(plaintext)
-for a in soup.select('#series--content > div:nth-child(1) > div > div > ul > li > div > div.index-list__title > a'):
-    dataPage = a['href']
-req = Request(dataPage, headers={'User-Agent': 'Mozilla/5.0'})
-html = urlopen(req).read()
-plaintext = html.decode('utf8')
-soup = BeautifulSoup(plaintext)
-for a in soup.select('.accordion__content > div > div > div > div > div > div > div.document__details > h3 > a'):
-    dataURL = a['href']
+flat = urlopen(req).read()
+plaintext = flat.decode('utf8')
+tree = html.fromstring(plaintext)
+link = tree.xpath('//*[@id="series--content"]/div[1]/div/div/ul/li/div/div[1]/a')[0].attrib['href']
+dataReq = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+dataFlat = urlopen(dataReq).read()
+dataPlaintext = dataFlat.decode('utf8')
+dataTree = html.fromstring(dataPlaintext)
+
+dataLink = dataTree.xpath('/html/body/div[2]/div/main/div/section/div[2]/div/article/section/div[4]/div/div[1]/div[3]/div/div/div/div/div/div/div/div/div[1]/h3/a')
+dataURL = dataLink[0].attrib['href']
 
 with open('info.json', 'r+') as info:
     data = json.load(info)
@@ -33,117 +40,136 @@ with open('info.json', 'r+') as info:
     json.dump(data, info, indent=4)
     info.truncate()
 
+
+# In[11]:
+
+
 scrape = Scraper(seed="info.json")
 scrape.distributions[0].title = "Attendance at local authority settings during the coronavirus (COVID-19) pandemic"
 scrape
 
-tabs = { tab.name: tab for tab in scrape.distributions[0].as_databaker() }
+
+# In[12]:
+
+
+scrape.distributions = [x for x in scrape.distributions]
+
+dist = scrape.distributions[0]
+display(dist)
+
+
+# In[13]:
+
+
+tabs = { tab.name: tab for tab in dist.as_databaker() if tab.name.startswith('Table')}
 list(tabs)
 
-next_table = pd.DataFrame()
 
-try :
-    tab = tabs['Table1_Eng']
-    cell = tab.filter('Date')
-    remove = tab.filter(contains_string('Source:'))
-    cell.assert_one()
-    authority = cell.fill(RIGHT).is_not_blank().is_not_whitespace()
-    date = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-    observations = authority.fill(DOWN).is_not_blank().is_not_whitespace() - remove
-    Dimensions = [
-                HDim(authority,'Local Authority Settings',DIRECTLY,ABOVE),
-                HDim(date, 'Period',DIRECTLY,LEFT),
-                HDimConst('Unit','Count'),
-                HDimConst('Measure Type','Attendence')
+# In[14]:
 
-    ]
-    c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
-    new_table = c1.topandas()
-    import numpy as np
-    new_table.rename(columns={'OBS': 'Value','DATAMARKER': 'Marker'}, inplace=True)
 
-    def user_perc1(x,y):
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
-            return 'Percentage'
-        else:
-            return y
-    new_table['Measure Type'] = new_table.apply(lambda row: user_perc1(row['Local Authority Settings'], row['Measure Type']), axis = 1)
+tidied_sheets = {}
 
-    def user_perc2(x,y):
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
-            return 'percent'
-        else:
-            return y
+for name, tab in tabs.items():
 
-    new_table['Unit'] = new_table.apply(lambda row: user_perc2(row['Local Authority Settings'], row['Unit']), axis = 1)
-    def date_time(time_value):
-        time_string = str(time_value).replace(".0", "").strip()
-        time_len = len(time_string)
-        if time_len == 10:
-            return 'gregorian-day/' + time_string[:10] + 'T00:00:00'
-    new_table["Period"] = new_table["Period"].apply(date_time)
-    next_table = pd.concat([next_table, new_table])
-except:
-    print ('Sheet not found')
+    if 'Table1_Eng' in name:
 
-# Period not defined properly due to variation in date format in source data
-# Further updated
+        remove = tab.filter(contains_string('Source:')).expand(DOWN).expand(RIGHT)
 
-try :
-    tab = tabs['Table2_Eng']
-    cell = tab.filter('Week beginning')
-    remove = tab.filter(contains_string('Source:'))
-    cell.assert_one()
-    authority = cell.shift(1,0).fill(RIGHT).is_not_blank().is_not_whitespace()
-    date = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-    observations = authority.fill(DOWN).is_not_blank().is_not_whitespace() - remove
-    Dimensions = [
-                HDim(authority,'Local Authority Settings',DIRECTLY,ABOVE),
-                HDim(date, 'Period',DIRECTLY,LEFT),
-                HDimConst('Unit','Count'),
-                HDimConst('Measure Type','Attendence')
+        cell = tab.filter('Week beginning')
 
-    ]
-    c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
-    new_table = c1.topandas()
-    import numpy as np
-    new_table.rename(columns={'OBS': 'Value','DATAMARKER': 'Marker'}, inplace=True)
+        week_beginning = cell.fill(DOWN).is_not_blank() - remove
 
-    def user_perc1(x,y):
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
-            return 'Percentage'
-        else:
-            return y
-    new_table['Measure Type'] = new_table.apply(lambda row: user_perc1(row['Local Authority Settings'], row['Measure Type']), axis = 1)
+        week_ending = week_beginning.shift(RIGHT)
 
-    def user_perc2(x,y):
-        if (str(x) ==  'Percentage of Settings') | (str(x) ==  'Percentage of children') | (str(x) ==  'Percentage of staff') :
-            return 'percent'
-        else:
-            return y
+        measurement = cell.shift(2, 0).expand(RIGHT).is_not_blank()
 
-    new_table['Unit'] = new_table.apply(lambda row: user_perc2(row['Local Authority Settings'], row['Unit']), axis = 1)
-    new_table['Period'] = new_table['Period'].str.rstrip('(a)')
-    def date_time(time_value):
-        time_string = str(time_value).replace(".0", "").strip()
-        time_len = len(time_string)
-        if time_len == 10:
-            return 'gregorian-day/' + time_string[:10] + 'T00:00/P4D'
-    new_table["Period"] = new_table["Period"].apply(date_time)
-    next_table = pd.concat([next_table, new_table])
-except:
-    print ('Sheet not found')
+        observations = week_ending.fill(RIGHT).is_not_blank()
+
+        dimensions = [
+        HDim(week_beginning, 'Week Beginning', DIRECTLY, LEFT),
+        HDim(week_ending, 'Week Ending', DIRECTLY, LEFT),
+        HDim(measurement, 'Measurement', DIRECTLY, ABOVE),
+        HDim(measurement, 'Measure Type', DIRECTLY, ABOVE),
+        HDim(measurement, 'Unit', DIRECTLY, ABOVE),
+        ]
+
+        #Measurement is used for meature tpye and unit here as I'm going
+        #to be using the measurement to determine each and this is the easier way
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        savepreviewhtml(tidy_sheet, fname="Preview.html")
+
+        tidied_sheets[name] = tidy_sheet.topandas()
+
+tidy_sheet.topandas()
+
+
+# In[15]:
+
+
+formatted_tables = {}
+
+for name in tidied_sheets:
+
+    if name == 'Table1_Eng':
+
+        df = tidied_sheets['Table1_Eng']
+
+        #df['Week Ending'] = df.apply(lambda x: 'gregorian-interval/' + str(parse(x['Week Ending']).date() - timedelta(days=6)) +'T00:00:00/P7D', axis = 1)
+
+        df = df.replace({'Measurement' : {
+                'Number of LAs that provided data (a)' : 'Number of LAs that provided data',
+                'Total number of pupils on roll at the LAs that provided data (b)' : 'Total number of pupils on roll at the LAs that provided data'},
+                         'Measure Type' : {
+                'Number of LAs that provided data (a)' : 'Local Authority',
+                'Number of pupils that did attend at least one session during the week' : 'Person',
+                'Number of pupils that were invited to attend at least one session during the week' : 'Person',
+                'Number of schools closed' : 'School',
+                'Number of schools open' : 'School',
+                'Percentage of all pupils on roll that did attend at least one session during the week' : 'Percentage',
+                'Percentage of pupils who were invited to attend that did attend at least one session during the week' : 'Percentage',
+                'Total number of pupils on roll at the LAs that provided data (b)' : 'Person'},
+                         'Unit' : {
+                'Number of LAs that provided data (a)' : 'Count',
+                'Number of pupils that did attend at least one session during the week' : 'Count',
+                'Number of pupils that were invited to attend at least one session during the week' : 'Count',
+                'Number of schools closed' : 'Count',
+                'Number of schools open' : 'Count',
+                'Percentage of all pupils on roll that did attend at least one session during the week' : 'Percent',
+                'Percentage of pupils who were invited to attend that did attend at least one session during the week' : 'Percent',
+                'Total number of pupils on roll at the LAs that provided data (b)' : 'Count'}})
+
+        df = df.rename(columns={'OBS':'Value'})
+
+        formatted_tables[name] = df
+
+
+# In[16]:
+
+
+df = pd.concat(formatted_tables.values(), ignore_index=True)
+
+df
+
+
+# In[17]:
+
 
 from IPython.core.display import HTML
-for col in new_table:
+for col in df:
     if col not in ['Value']:
-        next_table[col] = next_table[col].astype('category')
+        df[col] = df[col].astype('category')
         display(HTML(f"<h2>{col}</h2>"))
-        display(next_table[col].cat.categories)
+        display(df[col].cat.categories)
 
-tidy = next_table[['Period','Local Authority Settings', 'Measure Type','Unit',  'Value']]
+
+# In[18]:
+
+
+tidy = df[['Week Beginning', 'Week Ending', 'Measurement', 'Value', 'Measure Type', 'Unit']]
 
 out = Path('out')
 out.mkdir(exist_ok=True)
-tidy.drop_duplicates().to_csv(out / 'Attendance at local authority settings during the coronavirus COVID-19 pandemic_new.csv', index = False)
+tidy.drop_duplicates().to_csv(out / 'observations.csv', index = False)
 

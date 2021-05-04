8213df931c9d770737486ad95d4a24b25ed0f280
chunpang <chun.pang@ons.gov.uk>
2021-05-04 14:20:25 +0100

    Added initial dataset settings

--- datasets/ONS-Coronavirus-COVID-19-infections-in-the-community-in-England/main.py
+++ datasets/ONS-Coronavirus-COVID-19-infections-in-the-community-in-England/main.py
@@ -5,3 +5,21 @@
 import pandas as pd
 from gssutils import *
 import json
+
+info = json.load(open('info.json'))
+landingPage = info['landingPage']
+landingPage
+
+scraper = Scraper(seed='info.json')
+scraper
+
+trace = TransformTrace()
+cubes = Cubes('info.json')
+
+dist = scraper.distribution(latest=True)
+datasetTitle = info['title']
+dist
+datasetTitle
+
+tabs_name = ['1', '2a', '2b', '3a', '3b', '4a', '4b', '5a', '5b']
+tabs = {tab: tab for tab in dist.as_databaker() if tab.name in tabs_name}

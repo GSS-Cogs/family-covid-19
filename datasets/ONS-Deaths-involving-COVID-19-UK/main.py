ba1e2206501ce52a8cd9942ada0f476892b5fed5
chunpang <chun.pang@ons.gov.uk>
2021-06-16 16:18:28 +0100

    Added initial settings for transform

--- datasets/ONS-Deaths-involving-COVID-19-UK/main.py
+++ datasets/ONS-Deaths-involving-COVID-19-UK/main.py
@@ -5,3 +5,21 @@
 import pandas as pd
 from gssutils import *
 import json
+
+info = json.load(open('info.json'))
+landingPage = info['landingPage']
+landingPage
+
+scraper = Scraper(seed='info.json')
+scraper
+
+trace = TransformTrace()
+cubes = Cubes('info.json')
+
+dist = scraper.distribution(latest=True, mediaType=Excel)
+datasetTitle = info['title']
+dist
+datasetTitle
+
+tabs_name = ['Table 1', 'Table 2', 'Table 3', 'Table 4']
+tabs = {tab: tab for tab in dist.as_databaker() if tab.name in tabs_name}

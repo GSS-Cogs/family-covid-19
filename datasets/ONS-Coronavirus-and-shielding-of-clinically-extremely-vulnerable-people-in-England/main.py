2a310d2d38d0500172480631d30bb13bc92f7261
chunpang <chun.pang@ons.gov.uk>
2021-07-12 13:34:24 +0100

    Added initial dataset configuraations

--- datasets/ONS-Coronavirus-and-shielding-of-clinically-extremely-vulnerable-people-in-England/main.py
+++ datasets/ONS-Coronavirus-and-shielding-of-clinically-extremely-vulnerable-people-in-England/main.py
@@ -5,3 +5,21 @@
 import pandas as pd
 from gssutils import *
 import json
+
+info = json.load(open('info.json'))
+landingPage = info['landingPage']
+landingPage
+
+scraper = Scraper(seed='info.json')
+scraper
+
+trace = TransformTrace()
+cubes = Cubes('info.json')
+
+dist = scraper.distribution(latest=True, mediaType=Excel)
+datasetTitle = info['title']
+dist
+datasetTitle
+
+tabs_name = ['Contents', 'Definitions']
+tabs = {tab: tab for tab in dist.as_databaker() if tab.name not in tabs_name}

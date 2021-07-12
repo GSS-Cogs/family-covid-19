49a0f381afa2928ab6412b38f3a5aa7c24e09c96
chunpang <chun.pang@ons.gov.uk>
2021-07-12 16:41:13 +0100

    Added trace functions for initial configurations

--- datasets/ONS-Coronavirus-and-shielding-of-clinically-extremely-vulnerable-people-in-England/main.py
+++ datasets/ONS-Coronavirus-and-shielding-of-clinically-extremely-vulnerable-people-in-England/main.py
@@ -5,6 +5,7 @@
 import pandas as pd
 from gssutils import *
 import json
+import string
 
 info = json.load(open('info.json'))
 landingPage = info['landingPage']
@@ -23,3 +24,58 @@ datasetTitle
 
 tabs_name = ['Contents', 'Definitions']
 tabs = {tab: tab for tab in dist.as_databaker() if tab.name not in tabs_name}
+
+
+def left(s, amount):
+    return s[:amount]
+
+
+def right(s, amount):
+    return s[-amount:]
+
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+
+def cellLoc(cell):
+    return right(str(cell), len(str(cell)) - 2).split(" ", 1)[0]
+
+
+def col2num(col):
+    num = 0
+    for c in col:
+        if c in string.ascii_letters:
+            num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+    return num
+
+
+def colnum_string(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+
+def excelRange(bag):
+    xvalues = []
+    yvalues = []
+    for cell in bag:
+        coordinate = cellLoc(cell)
+        xvalues.append(''.join([i for i in coordinate if not i.isdigit()]))
+        yvalues.append(int(''.join([i for i in coordinate if i.isdigit()])))
+    high = 0
+    low = 0
+    for i in xvalues:
+        if col2num(i) >= high:
+            high = col2num(i)
+        if low == 0:
+            low = col2num(i)
+        elif col2num(i) < low:
+            low = col2num(i)
+        highx = colnum_string(high)
+        lowx = colnum_string(low)
+    highy = str(max(yvalues))
+    lowy = str(min(yvalues))
+    return '{' + lowx + lowy + '-' + highx + highy + '}'
